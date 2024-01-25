40294c7eb5e5792aefd40460850f2c015cd98ac8
Anders Bråthen Nomerstad <anders@nomerstad.com>
2024-01-25 17:12:48 +0100

    Leter i begge root-mappene dersom kallet til en static fil ikke kommer fra en av CMS-arkiv instansene

--- server/src/routing/cms-assets.ts
+++ server/src/routing/cms-assets.ts
@@ -1,32 +1,37 @@
-import { Express, Request } from 'express';
+import express, { Express, RequestHandler } from 'express';
 import path from 'path';
 
 const ASSETS_ROOT = path.join(process.cwd(), '..', 'cms-assets');
 const ASSETS_ROOT_SBS = path.join(ASSETS_ROOT, 'sbs');
 const ASSETS_ROOT_FSS = path.join(ASSETS_ROOT, 'fss');
 
-const assetsRoots: Record<string, string> = {
-    sbs: ASSETS_ROOT_SBS,
-    fss: ASSETS_ROOT_FSS,
+const staticSbs = express.static(ASSETS_ROOT_SBS);
+const staticFss = express.static(ASSETS_ROOT_FSS);
+
+const assetsRoots: Record<string, RequestHandler> = {
+    sbs: staticSbs,
+    fss: staticFss,
 } as const;
 
-const getAssetsRoot = (req: Request) => {
+// Attempt to use the correct assets root depending on the referrer-header of the caller
+// If the caller can not be determined from the referrer, we will look in both assets roots
+const staticHandlerByReferrer: RequestHandler = (req, res, next) => {
     const referrer = req.get('Referrer');
     if (!referrer) {
-        return null;
+        return next();
     }
 
-    const cmsInstance = new URL(referrer).pathname.split('/')[1];
-    return assetsRoots[cmsInstance];
+    const cmsInstanceFromPath = new URL(referrer).pathname.split('/')[1];
+    const staticReqHandler = assetsRoots[cmsInstanceFromPath];
+
+    return staticReqHandler ? staticReqHandler(req, res, next) : next();
 };
 
 export const setupCmsAssetsRouting = (expressApp: Express) => {
-    expressApp.use('/:siteKey/_public', (req, res, next) => {
-        const assetsRoot = getAssetsRoot(req);
-        if (!assetsRoot) {
-            return next();
-        }
-
-        res.sendFile(path.join(assetsRoot, req.path));
-    });
+    expressApp.use(
+        '/:siteKey/_public',
+        staticHandlerByReferrer,
+        staticSbs,
+        staticFss
+    );
 };
