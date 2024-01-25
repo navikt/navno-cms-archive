934b61bf02b3f3cc6358a8d83121c0663fe12539
Anders Bråthen Nomerstad <anders@nomerstad.com>
2024-01-25 17:16:40 +0100

    Comments etc

--- server/src/routing/cms-assets.ts
+++ server/src/routing/cms-assets.ts
@@ -8,13 +8,15 @@ const ASSETS_ROOT_FSS = path.join(ASSETS_ROOT, 'fss');
 const staticSbs = express.static(ASSETS_ROOT_SBS);
 const staticFss = express.static(ASSETS_ROOT_FSS);
 
-const assetsRoots: Record<string, RequestHandler> = {
+const staticReqHandlers: Record<string, RequestHandler> = {
     sbs: staticSbs,
     fss: staticFss,
 } as const;
 
 // Attempt to use the correct assets root depending on the referrer-header of the caller
 // If the caller can not be determined from the referrer, we will look in both assets roots
+// Normally, calls from an archive site will have either /sbs/* or /fss/* as referrer
+// CSS urls are one exception from this, they will instead have the url of the CSS file as their referrer
 const staticHandlerByReferrer: RequestHandler = (req, res, next) => {
     const referrer = req.get('Referrer');
     if (!referrer) {
@@ -22,9 +24,9 @@ const staticHandlerByReferrer: RequestHandler = (req, res, next) => {
     }
 
     const cmsInstanceFromPath = new URL(referrer).pathname.split('/')[1];
-    const staticReqHandler = assetsRoots[cmsInstanceFromPath];
+    const reqHandler = staticReqHandlers[cmsInstanceFromPath];
 
-    return staticReqHandler ? staticReqHandler(req, res, next) : next();
+    return reqHandler ? reqHandler(req, res, next) : next();
 };
 
 export const setupCmsAssetsRouting = (expressApp: Express) => {
