2150ffb3ab7d1f105aa8e578abe12799fb4dfaca
Anders Bråthen Nomerstad <anders@nomerstad.com>
2024-01-28 22:42:58 +0100

    Lenker til andre CMS-instansen

--- src/components/top-section/AppTopSection.tsx
+++ src/components/top-section/AppTopSection.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import { Heading } from '@navikt/ds-react';
+import { Heading, Link } from '@navikt/ds-react';
 
 import style from './AppTopSection.module.css';
+import { useAppState } from '../../state/useAppState.tsx';
 
 type Props = {
     cmsName: string;
 };
 
 export const AppTopSection = ({ cmsName }: Props) => {
+    const { appContext } = useAppState();
+
+    const otherCms = appContext.basePath === '/sbs' ? 'fss' : 'sbs';
+
     return (
         <div className={style.top}>
             <Heading size={'xlarge'} level={'1'}>
                 {`Arkiv - ${cmsName}`}
             </Heading>
+            <Link href={`/${otherCms}`}>{`Til ${otherCms.toUpperCase()}`}</Link>
         </div>
     );
 };
