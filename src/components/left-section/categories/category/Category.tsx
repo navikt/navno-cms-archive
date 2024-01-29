cf76899a88cf9b0210341ff37a79da745337416d
Anders Bråthen Nomerstad <anders@nomerstad.com>
2024-01-29 09:32:13 +0100

    Disable tom kategori

--- src/components/left-section/categories/category/Category.tsx
+++ src/components/left-section/categories/category/Category.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { CmsCategoryDocument } from '../../../../../common/cms-documents/category.ts';
-import { BodyLong, Loader } from '@navikt/ds-react';
-import { TreeItem, TreeItemContentProps, useTreeItem } from '@mui/x-tree-view';
+import { Loader } from '@navikt/ds-react';
+import { TreeItem } from '@mui/x-tree-view';
 import useSWRImmutable from 'swr/immutable';
 import { fetchCategories } from '../../../../utils/fetch/fetchCategories.ts';
 import { CategoriesList } from '../CategoriesList.tsx';
-import { classNames } from '../../../../utils/classNames.ts';
 import { useAppState } from '../../../../state/useAppState.tsx';
 import { fetchContent } from '../../../../utils/fetch/fetchContent.ts';
-import { FileTextIcon } from '@navikt/aksel-icons';
+import { CircleSlashIcon, FileTextIcon } from '@navikt/aksel-icons';
 
 import style from './Category.module.css';
 
@@ -22,19 +21,31 @@ export const Category = ({ category }: Props) => {
 
     const childKeys = categories.map((category) => category.key);
 
-    const { data, isLoading } = useSWRImmutable(
+    const { data: childCategories, isLoading } = useSWRImmutable(
         childKeys,
         fetchCategories(appContext.basePath)
     );
 
-    const label = `${title}${contents.length > 0 ? ` (${contents.length})` : ''}`;
+    const isEmpty =
+        !isLoading &&
+        (!childCategories || childCategories.length === 0) &&
+        contents.length === 0;
+
+    const label = `${title}${contents.length > 0 ? ` (${contents.length})` : isEmpty ? ' (tom)' : ''}`;
 
     return (
-        <TreeItem key={key} nodeId={key} label={label} className={style.item}>
+        <TreeItem
+            key={key}
+            nodeId={key}
+            label={label}
+            className={style.item}
+            disabled={isEmpty}
+            icon={isEmpty ? <CircleSlashIcon /> : undefined}
+        >
             {isLoading ? (
                 <Loader size={'xsmall'} />
-            ) : data ? (
-                <CategoriesList categories={data} />
+            ) : childCategories ? (
+                <CategoriesList categories={childCategories} />
             ) : null}
             {contents.map((content) => (
                 <TreeItem
@@ -57,68 +68,3 @@ export const Category = ({ category }: Props) => {
         </TreeItem>
     );
 };
-
-const CustomContent = React.forwardRef(function CustomContent(
-    props: TreeItemContentProps,
-    ref
-) {
-    const {
-        classes,
-        className,
-        label,
-        nodeId,
-        icon: iconProp,
-        expansionIcon,
-        displayIcon,
-    } = props;
-
-    const {
-        disabled,
-        expanded,
-        selected,
-        focused,
-        handleExpansion,
-        preventSelection,
-    } = useTreeItem(nodeId);
-
-    const icon = iconProp || expansionIcon || displayIcon;
-
-    const handleMouseDown = (
-        event: React.MouseEvent<HTMLDivElement, MouseEvent>
-    ) => {
-        preventSelection(event);
-    };
-
-    const handleExpansionClick = (
-        event: React.MouseEvent<HTMLDivElement, MouseEvent>
-    ) => {
-        handleExpansion(event);
-    };
-
-    return (
-        <div
-            className={classNames(className, classes.root, {
-                [classes.expanded]: expanded,
-                [classes.selected]: selected,
-                [classes.focused]: focused,
-                [classes.disabled]: disabled,
-            })}
-            onMouseDown={handleMouseDown}
-            ref={ref as React.Ref<HTMLDivElement>}
-        >
-            <div
-                onClick={handleExpansionClick}
-                className={classNames(classes.iconContainer, style.icon)}
-            >
-                {icon}
-            </div>
-            <BodyLong
-                onClick={handleExpansionClick}
-                as={'div'}
-                className={classes.label}
-            >
-                {label}
-            </BodyLong>
-        </div>
-    );
-});
