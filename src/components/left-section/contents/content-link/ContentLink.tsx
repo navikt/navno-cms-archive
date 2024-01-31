d25f30dd3025d07d4b86b85a92c4ebd1fc8802ff
Anders Bråthen Nomerstad <anders@nomerstad.com>
2024-01-31 14:54:25 +0100

    Uthever valgt innhold i innholdsvelger

--- src/components/left-section/contents/content-link/ContentLink.tsx
+++ src/components/left-section/contents/content-link/ContentLink.tsx
@@ -4,6 +4,7 @@ import { BodyShort, Link } from '@navikt/ds-react';
 import { useApiFetch } from '../../../../fetch/useApiFetch';
 import { useAppState } from '../../../../state/useAppState';
 import { ArrowRightIcon } from '@navikt/aksel-icons';
+import { classNames } from '../../../../utils/classNames';
 
 import style from './ContentLink.module.css';
 
@@ -12,14 +13,16 @@ type Props = {
 };
 
 export const ContentLink = ({ content }: Props) => {
-    const { setSelectedContent, appContext } = useAppState();
+    const { setSelectedContent, selectedContent, appContext } = useAppState();
     const { fetchContent } = useApiFetch();
+
+    const isSelected = selectedContent?.contentKey === content.contentKey;
 
     return (
         <Link
             key={content.contentKey}
             href={`${appContext.basePath}/${content.versionKey}`}
-            className={style.link}
+            className={classNames(style.link, isSelected && style.selected)}
             onClick={(e) => {
                 e.preventDefault();
                 fetchContent(content.contentKey).then((res) => {
