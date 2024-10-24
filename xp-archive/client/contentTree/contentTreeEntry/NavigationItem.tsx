15419029c88038bf64763fa5e0a39c8de46d269c
Anders Nomerstad <anders@nomerstad.com>
2024-10-24 11:05:06 +0200

    Bruker icon-url

--- xp-archive/client/contentTree/contentTreeEntry/NavigationItem.tsx
+++ xp-archive/client/contentTree/contentTreeEntry/NavigationItem.tsx
@@ -1,10 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { ContentTreeEntryData } from '../../../shared/types';
 import { useAppState } from '../../context/appState/useAppState';
 import { TreeItem } from '@mui/x-tree-view';
-import { FolderIcon } from '@navikt/aksel-icons';
 import { useContentTree } from 'client/hooks/useContentTree';
-
 
 type Props = {
     entry: ContentTreeEntryData;
@@ -13,14 +11,18 @@ type Props = {
 export const NavigationItem = ({ entry }: Props) => {
     const { setSelectedContentId } = useAppState();
 
-    const iconUrl = `${import.meta.env.VITE_APP_ORIGIN}/xp/api/contentIcon?type=${entry.type}`
-    const label = <span style={{ display: 'flex', alignItems: 'center' }}>
-        {/* <img src={`${import.meta.env.VITE_APP_ORIGIN}/xp/api/contentIcon?type=${entry.type}`} width={20} height={20} style={{ marginRight: '5px' }} /> */}
-        {entry.displayName}
-    </span>
+    const iconUrl = `${import.meta.env.VITE_APP_ORIGIN}/xp/api/contentIcon?type=${entry.type}`;
+    const label = (
+        <span style={{ display: 'flex', alignItems: 'center' }}>
+            <img src={iconUrl} width={20} height={20} style={{ marginRight: '5px' }} />
+            {entry.displayName}
+        </span>
+    );
 
     return (
-        <TreeItem itemId={entry.id} label={label}
+        <TreeItem
+            itemId={entry.id}
+            label={label}
             onClick={() => {
                 setSelectedContentId(entry.id);
             }}
@@ -34,9 +36,11 @@ const NavigationItems = ({ path }: { path: string }) => {
     const { data, isLoading } = useContentTree(path);
     return (
         <>
-            {isLoading ? <div>Loading...</div> :
-                data?.children.map(c => <NavigationItem entry={c} />)}
+            {isLoading ? (
+                <div>Loading...</div>
+            ) : (
+                data?.children.map((c) => <NavigationItem entry={c} />)
+            )}
         </>
     );
-}
-
+};
