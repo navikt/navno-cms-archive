8578702ccc03eba595c200a12e8c6daf30649429
Tale Prestmo <tale.prestmo@gmail.com>
2024-10-25 09:13:23 +0200

    Legg til json-view

--- xp-archive/client/contentJsonView/contentJsonView.tsx
+++ xp-archive/client/contentJsonView/contentJsonView.tsx
@@ -1,5 +1,18 @@
-export const ContentJsonView = () => {
-  return <div>
-    Se json her (Ikke implementert)
-  </div>
-}
+import React from 'react';
+import { collapseAllNested, JsonView } from 'react-json-view-lite';
+import { Content } from 'shared/types';
+import 'react-json-view-lite/dist/index.css';
+
+import style from './ContentJsonView.module.css';
+
+type Props = {
+    json: Content;
+};
+
+export const ContentJsonView = ({ json }: Props) => {
+    return (
+        <div className={style.wrapper}>
+            <JsonView data={json} clickToExpandNode={true} shouldExpandNode={collapseAllNested} />
+        </div>
+    );
+};
