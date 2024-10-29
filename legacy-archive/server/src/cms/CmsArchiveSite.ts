659dd4586083f7bf8808f849861d56a18f4c5e77
Anders Nomerstad <anders@nomerstad.com>
2024-10-29 12:36:15 +0100

    Refactor redirect fra internal

--- legacy-archive/server/src/cms/CmsArchiveSite.ts
+++ legacy-archive/server/src/cms/CmsArchiveSite.ts
@@ -116,15 +116,20 @@ export class CmsArchiveSite {
     }
 
     private setupSiteRoutes(router: Router, htmlRenderer: HtmlRenderer) {
-        router.get('/:versionKey?', cspMiddleware, async (req, res) => {
-            const rootCategories = this.categoriesService.getRootCategories();
-
-            const { hostname, protocol, baseUrl } = req;
+        // Redirect from internal urls
+        router.get('*', (req, res, next) => {
+            const { hostname, protocol, originalUrl } = req;
 
             if (hostname.endsWith(HOST_SUFFIX_INTERNAL)) {
-                const externalUrl = `${protocol}://${hostname.replace(HOST_SUFFIX_INTERNAL, HOST_SUFFIX_EXTERNAL)}${baseUrl}`;
+                const externalUrl = `${protocol}://${hostname.replace(HOST_SUFFIX_INTERNAL, HOST_SUFFIX_EXTERNAL)}${originalUrl}`;
                 return res.redirect(externalUrl);
             }
+
+            return next();
+        });
+
+        router.get('/:versionKey?', cspMiddleware, async (req, res) => {
+            const rootCategories = this.categoriesService.getRootCategories();
 
             const appContext = {
                 rootCategories,
