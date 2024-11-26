835b4ab3f6a0175ddb3aeff03ae61d1400e0b245
Tale Prestmo <tale.prestmo@gmail.com>
2024-11-26 09:22:07 +0100

    Endre datoformatering, legg inn combobox-variant

--- xp-archive/client/versionSelector/VersionSelector.tsx
+++ xp-archive/client/versionSelector/VersionSelector.tsx
@@ -1,9 +1,19 @@
-import React, { ChangeEvent } from 'react';
-import { Select } from '@navikt/ds-react';
+import React, { ChangeEvent, version } from 'react';
+import { Select, UNSAFE_Combobox } from '@navikt/ds-react';
 import { VersionReference } from 'shared/types';
 import { pruneString } from '@common/shared/pruneString';
 import { formatTimestamp } from '@common/shared/timestamp';
 import { useAppState } from 'client/context/appState/useAppState';
+
+// function getOptions(versions: VersionReference[]): { label: string; value: string }[] {
+//     const sisteVersjon = { label: 'Siste versjon', value: 'Siste versjon' };
+
+//     const mapVersionToOption = versions.map((v) => ({
+//         label: `${formatTimestamp(v.timestamp)} - ${pruneString(v.displayName, 100)}`,
+//         value: v.versionId,
+//     }));
+//     return [sisteVersjon, ...mapVersionToOption];
+// }
 
 type Props = {
     versions: VersionReference[];
@@ -21,18 +31,35 @@ export const VersionSelector = ({ versions }: Props) => {
         setSelectedVersion({ nodeId, versionId });
     };
 
+    // const selectVersionC = (versionId: string) => {
+    //     if (!versionId) {
+    //         setSelectedVersion(undefined);
+    //     }
+    //     const nodeId = versions.find((v) => v.versionId === versionId)?.nodeId;
+    //     setSelectedVersion({ nodeId, versionId });
+    // };
     return (
-        <Select
-            label={'Versjoner'}
-            onChange={selectVersion}
-            value={selectedVersion?.versionId ?? ''}
-        >
-            <option value={''}>Siste versjon</option>
-            {versions.map((version) => (
-                <option key={version.versionId} value={version.versionId}>
-                    {`${pruneString(version.displayName, 100)} - [${formatTimestamp(version.timestamp)}]`}
-                </option>
-            ))}
-        </Select>
+        <>
+            {/* <UNSAFE_Combobox
+                label={'Versjoner'}
+                onChange={selectVersionC}
+                value={selectedVersion?.versionId ?? 'Siste versjon'}
+                shouldAutocomplete={true}
+                options={getOptions(versions)}
+                clearButton={false}
+            ></UNSAFE_Combobox> */}
+            <Select
+                label={'Versjoner'}
+                onChange={selectVersion}
+                value={selectedVersion?.versionId ?? ''}
+            >
+                <option value={''}>Siste versjon</option>
+                {versions.map((version) => (
+                    <option key={version.versionId} value={version.versionId}>
+                        {`${formatTimestamp(version.timestamp)} - ${pruneString(version.displayName, 100)}`}
+                    </option>
+                ))}
+            </Select>
+        </>
     );
 };
