8ae5405059107719a62757d9a4088ce6faa30dac
bdahle <bdahle@hey.com>
2024-12-13 13:59:52 +0100

    gjør resultatene klikkbare

--- xp-archive/client/contentTree/search/SearchResult.tsx
+++ xp-archive/client/contentTree/search/SearchResult.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Loader } from '@navikt/ds-react';
 import { SearchResponse } from 'shared/types';
+import { useAppState } from 'client/context/appState/useAppState';
 
 type SearchResultProps = {
     isLoading: boolean;
@@ -9,17 +10,24 @@ type SearchResultProps = {
 };
 
 export const SearchResult = ({ isLoading, searchResult, query }: SearchResultProps) => {
+    const { setSelectedContentId } = useAppState();
     const { hits, total } = searchResult;
 
     return (
         <div>
             {isLoading ? (
-                <Loader />
+                <Loader size={'3xlarge'} />
             ) : (
                 <>
                     <div>{`Treff for "${query}" (${total}):`}</div>
                     {hits.map((hit) => (
-                        <div key={hit._id}>{hit.displayName}</div>
+                        <div
+                            key={hit._id}
+                            onClick={() => setSelectedContentId(hit._id)}
+                            style={{ cursor: 'pointer' }}
+                        >
+                            {hit.displayName}
+                        </div>
                     ))}
                 </>
             )}
