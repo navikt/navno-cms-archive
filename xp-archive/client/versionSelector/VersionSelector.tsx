e7087f28191f115969219165530f84d896e1cb80
bdahle <bdahle@hey.com>
2025-01-29 08:52:43 +0100

    slett gammel kommentert kode

--- xp-archive/client/versionSelector/VersionSelector.tsx
+++ xp-archive/client/versionSelector/VersionSelector.tsx
@@ -6,16 +6,6 @@ import { formatTimestamp } from '@common/shared/timestamp';
 import { useAppState } from 'client/context/appState/useAppState';
 import { updateContentUrl } from 'client/contentTree/contentTreeEntry/NavigationItem';
 
-// function getOptions(versions: VersionReference[]): { label: string; value: string }[] {
-//     const sisteVersjon = { label: 'Siste versjon', value: 'Siste versjon' };
-
-//     const mapVersionToOption = versions.map((v) => ({
-//         label: `${formatTimestamp(v.timestamp)} - ${pruneString(v.displayName, 100)}`,
-//         value: v.versionId,
-//     }));
-//     return [sisteVersjon, ...mapVersionToOption];
-// }
-
 type Props = {
     versions: VersionReference[];
 };
@@ -37,31 +27,14 @@ export const VersionSelector = ({ versions }: Props) => {
         setSelectedVersion(versionId);
     };
 
-    // const selectVersionC = (versionId: string) => {
-    //     if (!versionId) {
-    //         setSelectedVersion(undefined);
-    //     }
-    //     const nodeId = versions.find((v) => v.versionId === versionId)?.nodeId;
-    //     setSelectedVersion({ nodeId, versionId });
-    // };
     return (
-        <>
-            {/* <UNSAFE_Combobox
-                label={'Versjoner'}
-                onChange={selectVersionC}
-                value={selectedVersion?.versionId ?? 'Siste versjon'}
-                shouldAutocomplete={true}
-                options={getOptions(versions)}
-                clearButton={false}
-            ></UNSAFE_Combobox> */}
-            <Select label={'Versjoner'} onChange={selectVersion} value={selectedVersion ?? ''}>
-                <option value={''}>Siste versjon</option>
-                {versions.map((version) => (
-                    <option key={version.versionId} value={version.versionId}>
-                        {`${formatTimestamp(version.timestamp)} - ${pruneString(version.displayName, 100)}`}
-                    </option>
-                ))}
-            </Select>
-        </>
+        <Select label={'Versjoner'} onChange={selectVersion} value={selectedVersion ?? ''}>
+            <option value={''}>Siste versjon</option>
+            {versions.map((version) => (
+                <option key={version.versionId} value={version.versionId}>
+                    {`${formatTimestamp(version.timestamp)} - ${pruneString(version.displayName, 100)}`}
+                </option>
+            ))}
+        </Select>
     );
 };
