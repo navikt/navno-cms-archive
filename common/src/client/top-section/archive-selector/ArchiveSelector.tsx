e2d44957fffc373955d59f93c8a0e2b9084df0ef
bdahle <bdahle@hey.com>
2025-02-03 12:35:20 +0100

    test med onChange i dev

--- common/src/client/top-section/archive-selector/ArchiveSelector.tsx
+++ common/src/client/top-section/archive-selector/ArchiveSelector.tsx
@@ -13,7 +13,10 @@ export const ArchiveSelector = ({ basePath }: Props) => {
         <Tabs
             className={style.archiveSelector}
             defaultValue={basePath}
-            // onChange={(e) => window.location.assign(e.target.value)}
+            onChange={(e) => {
+                console.log('Tab change event:', e);
+                window.location.assign(e);
+            }}
         >
             <Tabs.List>
                 {siteConfigs.map((config) => (
