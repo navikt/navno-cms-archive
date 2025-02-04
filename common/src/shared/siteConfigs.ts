4bd154f72dd37b5334e215cd67c194f38e54ce6b
bdahle <bdahle@hey.com>
2025-02-04 10:36:16 +0100

    årstall på Navet-fanen

--- common/src/shared/siteConfigs.ts
+++ common/src/shared/siteConfigs.ts
@@ -22,7 +22,7 @@ export const legacyArchiveConfigs: LegacyArchiveSiteConfig[] = [
         type: 'enonic-legacy',
     },
     {
-        name: 'Navet',
+        name: 'Navet før 2019',
         basePath: '/fss',
         indexPrefix: 'cmsfss',
         type: 'enonic-legacy',
