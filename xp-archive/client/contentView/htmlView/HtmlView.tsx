92e82b07bc157007f17b2c5722a68d6469d1963b
Tale Prestmo <tale.prestmo@gmail.com>
2025-03-19 09:08:17 +0100

    Add alert for layerredirect

--- xp-archive/client/contentView/htmlView/HtmlView.tsx
+++ xp-archive/client/contentView/htmlView/HtmlView.tsx
@@ -2,22 +2,30 @@ import React, { useState } from 'react';
 import style from './HtmlView.module.css';
 import { xpArchiveConfig } from '@common/shared/siteConfigs';
 import { Alert, Loader } from '@navikt/ds-react';
+import { Content } from '../../../shared/types';
 
 type Props = {
-    nodeId: string;
+    content: Content;
     locale: string;
-    versionId: string;
-    originalContentTypeName: string | undefined;
+};
+const localeNames: Record<string, string> = {
+    no: 'norsk (bokmål)',
+    nn: 'nynorsk',
+    en: 'engelsk',
+    se: 'samisk',
 };
 
-export const HtmlView = ({ nodeId, locale, versionId, originalContentTypeName }: Props) => {
+export const HtmlView = ({ content, locale }: Props) => {
     const [isLoading, setIsLoading] = useState(true);
-    const htmlPath = `${xpArchiveConfig.basePath}/html/${nodeId}/${locale}/${versionId}`;
+    const htmlPath = `${xpArchiveConfig.basePath}/html/${content._id}/${locale}/${content._versionKey}`;
 
     return (
         <div className={style.wrapper}>
-            {originalContentTypeName ? (
-                <Alert variant="warning">{`Obs! Denne siden var opprinnelig en "${originalContentTypeName}" og inneholder versjonshistorikk.`}</Alert>
+            {content.originalContentTypeName ? (
+                <Alert variant="warning">{`Obs! Denne siden var opprinnelig en "${content.originalContentTypeName}" og inneholder versjonshistorikk.`}</Alert>
+            ) : null}
+            {content.x['no-nav-navno'].redirectToLayer.locale ? (
+                <Alert variant="warning">{`Obs! Denne siden er satt som redirect til språkversjonen for "${localeNames[content.x['no-nav-navno'].redirectToLayer.locale]}". Husk å velge riktig språkversjon for å se korrekt historikk.`}</Alert>
             ) : null}
             {isLoading && (
                 <div className={style.loaderWrapper}>
