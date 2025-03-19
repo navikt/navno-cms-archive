c67b02440a58917bd864f353506c71ee17434ab0
Tale <tale.prestmo@gmail.com>
2025-03-19 09:12:50 +0100

    Revert "Upgrade mar"

--- legacy-archive/client/components/left-section/categories/CategoriesMenu.tsx
+++ legacy-archive/client/components/left-section/categories/CategoriesMenu.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { CmsCategoryListItem } from '../../../../shared/cms-documents/category';
-import { SimpleTreeView } from '@mui/x-tree-view';
+import { ChevronDownIcon, ChevronRightIcon } from '@navikt/aksel-icons';
+import { TreeView } from '@mui/x-tree-view';
 import { RootCategory } from './root-category/RootCategory';
 import { CheckboxGroup } from '@navikt/ds-react';
 import { useSearchState } from '../../../context/search-state/useSearchState';
@@ -14,7 +15,9 @@ export const CategoriesMenu = ({ rootCategories, className }: Props) => {
     const { updateSearchParams, searchParams } = useSearchState();
 
     return (
-        <SimpleTreeView
+        <TreeView
+            defaultExpandIcon={<ChevronRightIcon />}
+            defaultCollapseIcon={<ChevronDownIcon />}
             classes={{
                 root: className,
             }}
@@ -29,6 +32,6 @@ export const CategoriesMenu = ({ rootCategories, className }: Props) => {
                     <RootCategory category={category} key={category.key} />
                 ))}
             </CheckboxGroup>
-        </SimpleTreeView>
+        </TreeView>
     );
 };
