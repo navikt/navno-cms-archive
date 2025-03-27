4ee817466fc49c6dfaf2154902b11cbab8efac88
Tale Prestmo <tale.prestmo@gmail.com>
2025-03-27 09:15:00 +0100

    useeffect for path

--- xp-archive/client/appDataComponent/AppDataComponent.tsx
+++ xp-archive/client/appDataComponent/AppDataComponent.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SideView } from '../sideView/SideView';
 import { Content } from '../content/Content';
 import { useFetchContent } from '../hooks/useFetchContent';
 import { useAppState } from '../context/appState/useAppState';
 
 export const AppDataComponent = () => {
-    const { selectedContentId, selectedLocale, selectedVersion } = useAppState();
+    const { selectedContentId, selectedLocale, selectedVersion, updateSelectedContent } =
+        useAppState();
+
+    useEffect(() => {
+        const pathSegments = window.location.pathname.split('/');
+        const contentId = pathSegments?.[2];
+        const locale = pathSegments?.[3];
+        const versionId = pathSegments?.[4];
+        if (contentId && contentId !== selectedContentId) {
+            updateSelectedContent({ contentId, locale, versionId: versionId || undefined });
+        }
+    }, [selectedContentId]);
     const { data, isLoading } = useFetchContent({
         id: selectedContentId ?? '',
         locale: selectedLocale ?? 'no',
