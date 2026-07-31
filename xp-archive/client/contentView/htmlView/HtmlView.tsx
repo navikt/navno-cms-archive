b7f472afe976f07618e7beefe7728148df84e062
Tale Prestmo <tale.prestmo@gmail.com>
2026-07-31 09:36:22 +0200

    legg til info om avpubliseringstidspunkt ved  endring til intern lenke

--- xp-archive/client/contentView/htmlView/HtmlView.tsx
+++ xp-archive/client/contentView/htmlView/HtmlView.tsx
@@ -2,10 +2,31 @@ import React, { useState } from 'react';
 import style from './HtmlView.module.css';
 import { xpArchiveConfig } from '@common/shared/siteConfigs';
 import { Alert, Loader } from '@navikt/ds-react';
-import { Content } from '../../../shared/types';
+import { Content, VersionReference } from '../../../shared/types';
+import { formatTimestamp } from '../../../../common/src/shared/timestamp';
 
 type Props = {
     content: Content;
+    versions: VersionReference[];
+};
+
+const getUnpublishedWarningText = (content: Content, versions: VersionReference[]) => {
+    if (content.originalContentTypeName && content.type === 'no.nav.navno:internal-link') {
+        const v = versions.find((version, index, versions) => {
+            const nextVersion = versions[index + 1];
+            return (
+                version.type === 'no.nav.navno:internal-link' &&
+                !!nextVersion &&
+                nextVersion.type !== 'no.nav.navno:internal-link'
+            );
+        });
+
+        return v
+            ? `Siden ble endret til en intern lenke med videresending til nytt innhold den ${formatTimestamp(v.timestamp)}. Fra dette tidspunktet var innholdet ikke lenger tilgjengelig på nav.no.`
+            : '';
+    }
+
+    return '';
 };
 
 const localeNames: Record<string, string> = {
@@ -15,14 +36,14 @@ const localeNames: Record<string, string> = {
     se: 'samisk',
 };
 
-export const HtmlView = ({ content }: Props) => {
+export const HtmlView = ({ content, versions }: Props) => {
     const [isLoading, setIsLoading] = useState(true);
     const htmlPath = `${xpArchiveConfig.basePath}/html/${content._id}/${content.locale}/${content._versionKey}`;
 
     return (
         <div className={style.wrapper}>
             {content.originalContentTypeName ? (
-                <Alert variant="warning">{`Obs! Denne siden var opprinnelig en "${content.originalContentTypeName}" og inneholder versjonshistorikk.`}</Alert>
+                <Alert variant="warning">{`Obs! Denne siden var opprinnelig en "${content.originalContentTypeName}" og inneholder versjonshistorikken. ${getUnpublishedWarningText(content, versions)}`}</Alert>
             ) : null}
             {content.x?.['no-nav-navno']?.redirectToLayer?.locale ? (
                 <Alert variant="warning">{`Obs! Denne siden er satt som redirect til språkversjonen for "${localeNames[content.x['no-nav-navno'].redirectToLayer.locale]}". Husk å velge riktig språkversjon for å se korrekt historikk.`}</Alert>
